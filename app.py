593973e04e4af3a660cbd808cb0684e7809d7087
G. Schwarzman <g.schwarzman@hotmail.com>
2016-03-15 15:19:25 +0200

    rewrite

--- app.py
+++ app.py
@@ -2,17 +2,11 @@ def application(env, start_response):
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
-    response_body = process_request(env['REQUEST_METHOD'],'')
-    return repr(response_body)
-
-
-
-def process_request(request_method, request_body):
-    response_body = []
+    request_method = env['REQUEST_METHOD']
     if request_method == 'GET':
-        response_body.append('received GET request')
+        response_body = 'received GET request'
     elif request_method == 'POST':
-        response_body.append('received POST request')
+        response_body = 'received POST request'
     else:
-        response_body.append('request method is not implemented')
+        response_body = 'request method is not implemented'
     return response_body
