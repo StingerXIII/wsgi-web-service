d7ee79965fdb1fc119498138d8365ac44a5114fd
G. Schwarzman <g.schwarzman@hotmail.com>
2016-03-23 23:52:39 +0200

    select last record from db implemented

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from app_config import db_parms
+import psycopg2
+
+
+# db_parms syntax:
+# db_parms = {
+#     'db_name': '',
+#     'login': '',
+#     'password':  '',
+#     'host': '',
+#     'port': ''
+# }
 
 def application(env, start_response):
     status = '200 OK'
@@ -8,7 +20,7 @@ def application(env, start_response):
     if request_method == 'GET':
         response_body = 'received GET request\n'
         if env['PATH_INFO'] == '/last':
-            response_body = response_body + get_last_record()
+            response_body += get_last_record()
     elif request_method == 'POST':
         response_body = 'received POST request\n' + 'request body is:\n' + request_body
     else:
@@ -17,4 +29,20 @@ def application(env, start_response):
 
 
 def get_last_record():
-    pass
+    message = ""
+    query = """select rec_text from records
+    where rec_date = (select max(rec_date) from records)
+    and rec_time = (select max(rec_time) from records)"""
+    conn = psycopg2.connect("dbname="+db_parms.get('db_name')+
+                            " user="+db_parms.get('login')+
+                            " host="+db_parms.get('host')+
+                            " password="+db_parms.get('password'))
+    cur=conn.cursor()
+    cur.execute(query)
+    result = cur.fetchall()
+    for row in result:
+        message = message + row[0]
+    return message
+
+
+    return
